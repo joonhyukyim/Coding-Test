f6bf58bb137cdeda4999e40a978cf0200a3f26e9
Burnt-Waffle <michael568@hanmail.net>
2026-06-24 14:31:56 +0900

    260624

---
+++ programmers/lv2/154539.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+def solution(numbers):
+    answer = [-1] * len(numbers)
+    stack = []
+    
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            index = stack.pop()
+            answer[index] = numbers[i]
+            
+        stack.append(i)
+    return answer
